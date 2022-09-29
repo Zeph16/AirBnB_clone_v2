2207458fc3ef8d32844c77cbd1765d5d69db4e55
Sofonias Ayele <sof16ayele@gmail.com>
2022-09-29 23:55:14 +0300

    Minor fix

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -2,55 +2,55 @@
 
 exec {'update':
   provider => shell,
-  command  => 'sudo apt-get -y update',
+  command  => 'apt-get -y update',
   before   => Exec['install Nginx'],
 }
 
 exec {'install Nginx':
   provider => shell,
-  command  => 'sudo apt-get -y install nginx',
+  command  => 'apt-get -y install nginx',
   before   => Exec['start Nginx'],
 }
 
 exec {'start Nginx':
   provider => shell,
-  command  => 'sudo service nginx start',
+  command  => 'service nginx start',
   before   => Exec['create first directory'],
 }
 
 exec {'create first directory':
   provider => shell,
-  command  => 'sudo mkdir -p /data/web_static/releases/test/',
+  command  => 'mkdir -p /data/web_static/releases/test/',
   before   => Exec['create second directory'],
 }
 
 exec {'create second directory':
   provider => shell,
-  command  => 'sudo mkdir -p /data/web_static/shared/',
+  command  => 'mkdir -p /data/web_static/shared/',
   before   => Exec['content into html'],
 }
 
 exec {'content into html':
   provider => shell,
-  command  => 'echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html',
+  command  => 'echo "Hello World!" | tee /data/web_static/releases/test/index.html',
   before   => Exec['symbolic link'],
 }
 
 exec {'symbolic link':
   provider => shell,
-  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
   before   => Exec['put location'],
 }
 
 exec {'put location':
   provider => shell,
-  command  => 'sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' /etc/nginx/sites-available/default',
+  command  => 'sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' /etc/nginx/sites-available/default',
   before   => Exec['restart Nginx'],
 }
 
 exec {'restart Nginx':
   provider => shell,
-  command  => 'sudo service nginx restart',
+  command  => 'service nginx restart',
   before   => File['/data/']
 }
 
