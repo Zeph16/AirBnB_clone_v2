61a88f0ae81de3e97a030274da6342720de099d5
Sofonias Ayele <sof16ayele@gmail.com>
2022-10-13 18:50:58 +0300

    Tasks 1 - 4

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Script that runs a flask web app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ returns content for /route """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ returns content for /hbnb """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ returns content for /c/<text> """
+    return 'C %s' % text.replace('_', ' ')
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Script that runs a flask web app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ returns content for /route """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ returns content for /hbnb """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ returns content for /c/<text> """
+    return 'C %s' % text.replace('_', ' ')
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="cool"):
+    """ returns content for /python/<text> """
+    return 'Python %s' % text.replace('_', ' ')
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ returns content for /number/<n> route """
+    return "%d is a number" % n
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
