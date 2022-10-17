5840b9f8f40efed6bd44ba5279b10873c1e453ff
Sofonias Ayele <sof16ayele@gmail.com>
2022-10-17 01:02:20 +0000

    Task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
-""" Script that runs a flask web app """
-from flask import Flask, render_template
+"""
+starts a Flask web application
+"""
 from models import storage
-from models.state import State
-from models.city import City
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
+@app.route("/cities_by_states", strict_slashes=False)
+def citystates():
+    ''' Display page with list of all states and related cities '''
+    states = storage.all("State")
+    return render_template("9-states.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ 'tears down' the sql alchemy session """
+def teardown(exc):
+    ''' reset the session of db storage '''
     storage.close()
 
 
-@app.route('/states/', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def display_html():
-    """ provides content for the /states route """
-    states = storage.all(State)
-
-    if not id:
-        dict_to_html = {value.id: value.name for value in states.values()}
-        return render_template('7-states_list.html',
-                               Table="States",
-                               items=dict_to_html)
-
-    k = "State.{}".format(id)
-    if k in states:
-        return render_template('9-states.html',
-                               Table="State: {}".format(states[k].name),
-                               items=states[k])
-
-    return render_template('9-states.html',
-                           items=None)
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port='5000')
